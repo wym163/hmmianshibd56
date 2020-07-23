e04f85733c69a230cf357f2360cc3495ba45738d
wym163 <2529665764@qq.com>
2020-07-23 20:40:33 +0800

    vant 配置 rem 自动设置html字体大小

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入router
 import router from './router'
+// 导入store
 import store from './store'
+// 导入normalize.css
 import 'normalize.css'
+// 导入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// 导入flexible
+import 'amfe-flexible'
 Vue.use(Vant)
 Vue.config.productionTip = false
 
